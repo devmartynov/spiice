78303b5b49179e9e74582cb034a1aa45a6ca41c2
dmartynov <denis.martynov.93@mail.ru>
2022-10-01 19:31:19 +0300

    add transition between screens logic

---
+++ app/src/main/java/io/devmartynov/spiice/EnjoyFeatureActivity.kt
@@ -0,0 +1,32 @@
+package io.devmartynov.spiice
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class EnjoyFeatureActivity : AppCompatActivity() {
+    private val timer: Timer = TransitionTimer.get()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_enjoy_feature)
+        timer.start(::goToSignUp)
+        findViewById<Button>(R.id.skip).setOnClickListener(::onSkipClick)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        timer.start(::goToSignUp)
+    }
+
+    private fun onSkipClick(view: View) {
+        timer.stop()
+        goToSignUp()
+    }
+
+    private fun goToSignUp() {
+        startActivity(Intent(this, SignUpActivity::class.java))
+    }
+}
---
+++ app/src/main/java/io/devmartynov/spiice/ProjectsFeatureActivity.kt
@@ -0,0 +1,36 @@
+package io.devmartynov.spiice
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class ProjectsFeatureActivity : AppCompatActivity() {
+    private val timer: Timer = TransitionTimer.get()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_projects_feature)
+        timer.start(::goToNextScreen)
+        findViewById<Button>(R.id.skip).setOnClickListener(::onSkipClick)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        timer.start(::goToNextScreen)
+    }
+
+    private fun onSkipClick(view: View) {
+        timer.stop()
+        goToSignUp()
+    }
+
+    private fun goToNextScreen() {
+        startActivity(Intent(this, MoneyFeatureActivity::class.java))
+    }
+
+    private fun goToSignUp() {
+        startActivity(Intent(this, SignUpActivity::class.java))
+    }
+}
