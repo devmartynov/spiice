ce3c0ddbbe31e1b581d23cac4934c4e6ac6124fc
dmartynov <denis.martynov.93@mail.ru>
2022-10-17 12:37:48 +0300

    add interface comment

--- app/src/main/java/io/devmartynov/spiice/ValidationResult.kt
+++ app/src/main/java/io/devmartynov/spiice/ValidationResult.kt
@@ -1,6 +1,17 @@
 package io.devmartynov.spiice
 
+/**
+ * Результат валидации.
+ */
 interface ValidationResult {
+    /**
+     * Есть ли ошибки валидации?
+     * @return true если ошибок нет, иначе false
+     */
     fun hasErrors(): Boolean
+    /**
+     * Ошибки валидации
+     * @return если ошибки есть, то список ошибок, иначе пустой список
+     */
     fun getErrors(): List<String>
 }
